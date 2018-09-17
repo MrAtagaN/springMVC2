4b493253ec53b9a255673f7fb74872f2f97bfb06
plekhanovsa <atagan@rambler.ru>
2018-09-17 12:19:54 +0300

    first

--- src/main/java/com/atagan/controller/UserController.java
+++ src/main/java/com/atagan/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.atagan.model.User;
@@ -18,6 +19,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public @ResponseBody
     List<User> getAllUsers() {
            return userService.getAllUsers();
